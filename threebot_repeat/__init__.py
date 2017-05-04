4a88560324a100393238a0104b65fa48c4559b8e
Walter Renner <renner@arteria.ch>
2017-05-04 17:35:55 +0200

    added app sources

---
+++ threebot_repeat/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import checks
+
+__version__ = '0.1.0'
+
+default_app_config = 'threebot_repeat.apps.DefaultConfig'
---
+++ threebot_repeat/urls.py
@@ -0,0 +1,18 @@
+"""URLs for the threebot_repeat app."""
+from django.conf.urls import patterns, url
+
+from threebot_repeat import views
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^$',
+        views.list,
+        name='threebot_repeat_list'),
+    url(r'^repeat-and-replay-log/(?P<log_id>[-\d]+)/$',
+        views.replay_and_repeat,
+        name='threebot_replay_and_repeat', ),
+    url(r'^stop-repetition/(?P<bg_task_id>[-\d]+)/$',
+        views.stop_repetition,
+        name='threebot_replay_stop_repetition', ),
+)
